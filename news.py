ea661edb3074a4e94f64389ec2437b2be5e26adc
RichardKim <hiddeneventor@gmail.com>
2020-03-18 15:54:33 +0900

    코로나 sbs 최신 뉴스 파싱 완료 aws에 반영하기

--- news.py
+++ news.py
@@ -3,30 +3,39 @@ import urllib.request
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-url = 'http://ncov.mohw.go.kr/'
+# sbs 뉴스기준 최신 코로나 뉴스
+url = "https://news.sbs.co.kr/news/newsHotIssueList.do?tagId=10000050973"
+base_url = "https://news.sbs.co.kr"
 soup = BeautifulSoup(urllib.request.urlopen(url).read().decode('utf-8'), "html.parser")
-data1 = soup.find('ul', {'class': 'liveNum'})
-data1 = data1.find_all('li')
+data1 = soup.find_all('a', {'class': 'news'})
+
+midleRoot = {}
 list = []
 
-# def addCoronaList(item) :
-midleRoot = {}
-
-
-# 대분류 환자현황
+# 대분류 sbs_news
 for item in data1:
-    # 중분류 [0]확진환자, [1]완치, [2]치료중, [3]사망
     patientInfo_dic = {}
-    title = item.find('strong', 'tit').get_text()
-    totNum = item.find('span', 'num').get_text()
-    beforeNum = item.find('span', 'before').get_text()
-    patientInfo_dic["title"] = title
-    patientInfo_dic["tot_num"] = totNum
-    patientInfo_dic["before_num"] = beforeNum
+
+#    print(title)
+    # totNum = item.find('span', 'num').get_text()
+    articleUrl = item.get("href")
+    articleUrl = base_url+articleUrl
+
+    title = item.get("title")
+    img = item.find("img")  # 이미지 태그
+    img_src = img.get("data-src")  # 상대 이미지 경로
+    content = item.find("span", {"class": "read"}).get_text()
+    date = item.find("span", {"class": "date"}).get_text()
+    # print("\n")
+    # print(img_src)
+    patientInfo_dic["article_title"] = title
+    patientInfo_dic["article_url"] = articleUrl
+    patientInfo_dic["article_img_src"] = img_src
+    patientInfo_dic["article_content"] = content
+    patientInfo_dic["write_date"] = date
     list.append(patientInfo_dic)
 
-midleRoot["patient_status"] = list
+midleRoot["sbs_news"] = list
 
-# Print JSON
-#print(json.dumps(midleRoot, ensure_ascii=False, indent="\t"))
 print(json.dumps(midleRoot))
+# print(json.dumps(midleRoot, ensure_ascii=False, indent="\t"))
